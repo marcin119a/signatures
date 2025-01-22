486a0fe581b0b879ad403f9c39f00c5459df61eb
Marcin Wierzbiński <marcin119a@gmail.com>
2025-01-22 13:05:02 +0100

    modify

--- assigment.py
+++ assigment.py
@@ -1,5 +1,5 @@
 from SigProfilerAssignment import Analyzer as Analyze
-Analyze.cosmic_fit('/Users/mw/PycharmProjects/get_vcsv/data/genomic_mutations/output/SBS/signatures.SBS96.all', 'data/signatures',
+Analyze.cosmic_fit('output/signatures.SBS96.all', 'data/signatures',
                    input_type="matrix",
                    context_type="96",
                    collapse_to_SBS96=True, cosmic_version=3.4, exome=False,
